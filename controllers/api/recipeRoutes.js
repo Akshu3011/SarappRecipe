0bc70cc0eb92a1ebefae8a087a86213faaf3188d
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2023-02-06 17:26:22 -0800

    adding project

---
+++ controllers/api/recipeRoutes.js
@@ -0,0 +1,123 @@
+const router = require('express').Router();
+const { User, Recipe } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+const cloudinary = require("cloudinary").v2;
+
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+
+//ak- Routes
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    // async code using `req` and `file`
+    // ...
+    return {
+      folder: 'sarapp_img',
+      format: 'jpeg',
+      public_id: 'recipe_img',
+    };
+  },
+});
+
+
+const upload = multer({ storage: storage });
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all recipes and JOIN with user data
+    const recipeData = await Recipe.findAll({
+      
+    });
+
+    res.status(200).json(recipeData);
+  } catch(err){
+    res.status(400).json(err);
+  };
+ 
+});
+/*
+router.post('/img', upload.single("picture"), async (req, res) => {
+  return res.json({ picture: req.file.path });
+});*/
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newRecipe = await Recipe.create({
+      ...req.body,
+     // 'img_url':req.file.path, 
+      UserId: req.session.user_id,
+    });
+
+    res.status(200).json(newRecipe);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+
+  }
+});
+
+// create new recipe
+// url: port/api/recipes
+router.put('/', withAuth, async (req, res) => {
+  try {
+    const newRecipe = await Recipe.create({
+      ...req.body,
+      UserId: req.session.user_id,
+    });
+
+    res.status(200).json(newRecipe);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// edit existing recipe
+// url: port/api/recipes/ID NUMBER
+// pause until MVP is done
+router.put('/:id', async (req,res)=>{
+  try{
+    const editRecipe = await Recipe.update(
+      {
+        dish_name: req.body.recipe_name,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        regions: req.body.regions,
+        dietary: req.body.dietary,
+        img_url: req.body.img_url
+      },
+      {
+        where: {
+          id: req.params.id,
+        }
+      });
+      res.status(200).json(dish);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const recipeData = await Recipe.destroy({
+      where: {
+        id: req.params.id,
+        UserId: req.session.user_id,
+      },
+    });
+
+    if (!recipeData) {
+      res.status(404).json({ message: 'No recipe with this id!' });
+      return;
+    }
+
+    res.status(200).json(recipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ public/js/postRecipe.js
@@ -0,0 +1,105 @@
+/*let addIngredientsBtn = document.getElementById('addIngredientsBtn');
+let ingredientList = document.querySelector('.ingredientList');
+let ingredeintDiv = document.querySelectorAll('.ingredeintDiv')[0];
+
+
+var ingredientsArray=[];
+addIngredientsBtn.addEventListener('click', function(){
+  ingredientsArray.push(document.getElementById('ingredients').value);
+  let newIngredients = ingredeintDiv.cloneNode(true);
+  let input = newIngredients.getElementsByTagName('input')[0];
+  input.value = '';
+  ingredientList.appendChild(newIngredients);
+});*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+
+
+
+var imgurl="";
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dbusqnlkm', 
+    uploadPreset: 'axaukuzt'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result); 
+       
+      console.log("ak ", result.info.secure_url);
+     imgurl= result.info.secure_url;
+      }
+    }
+  )
+
+
+        console.log("s" +imgurl);
+//selected region
+var region="";
+function getSelectValue(){
+        region = document.getElementById('region').value;
+			
+}
+  //      console.log(region); 
+
+    
+  
+  var checkboxes=document.querySelectorAll('.dietary');
+  var listArray=[];
+
+  for(var checkbox of checkboxes)
+  {
+      checkbox.addEventListener('click',function(){
+        if(this.checked == true)
+        {
+          console.log(this.value);
+          listArray.push(this.value);
+        }
+        else{
+          console.log("You unchecked");
+          listArray =listArray.filter(e=> e !== this.value)
+        }
+      })
+  }
+ 
+
+
+const addnewrecipe= (e) =>{
+   
+        e.preventDefault();
+     
+        console.log("submit");
+        const recipeObj = {
+            dish_name:document.querySelector("#dish_name").value,
+            instructions:document.querySelector("#instructions").value,
+            ingredients:document.querySelector("#ingredients").value,
+            regions:region,
+            dietary:listArray.toString(),
+            img_url:imgurl  
+        }
+    
+        fetch("/api/recipes",{
+            method:"POST",
+            body:JSON.stringify(recipeObj),
+            headers:{
+                "Content-Type":"application/json"
+               
+            }
+        }).then(res=>{
+            if(res.ok){
+              alert("Added a new recipe successfully!!!")
+              
+               location.reload();
+            } else {
+                alert("Error posting recipe")
+            }
+        })
+        console.log("akchoices "+listArray);
+
+}
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+document.querySelector("#newRecipeForm").addEventListener("submit",addnewrecipe);
+
